1b95c54b0000b4f36f7ecd183592a0407c19e9f2
loic1805 <loicgasikara@gmail.com>
2025-12-04 00:23:50 +0100

    fixed variable naming bug to correctly plot drivers

--- src/plotter.py
+++ src/plotter.py
@@ -5,28 +5,29 @@ import seaborn as sns
 #let's apply the basic f1 styling
 fastf1.plotting.setup_mpl()
 
-def plotSpeedTrace(driver1Tel, driver1Code, driver2Tel, driver2Code, grandPrixName):
+def plotSpeedTrace(session, driver1Tel, driver1Code, driver2Tel, driver2Code):
     """
     Plots a comparison of speed traces for two drivers
     
+    :param session (object): the fastF1 session object
     :param driver1Tel (dataframe): driver 1's telemetry data
     :param driver1Code (str): driver 1's code (e.g 'VER')
     :param driver2Tel (dataframe): driver 2's telemetry data
     :param driver2Code (str): driver 2's code (e.g 'LEC')
-    :param grandPrixName (str): the chart's title
     """
+    eventName = f"{session.event.EventName} {session.event.year}"
     print(f"Generating chart: {driver1Code} vs {driver2Code}..")
     #first we create the main figure
     fig, ax = plt.subplots(figsize=(12, 6))
-    ax.set_title(f"{grandPrixName} : Speed comparison", fontsize=16)
+    ax.set_title(f"{eventName} : Speed comparison", fontsize=16)
 
     #then we plot each driver
     #driver 1
-    color1 = fastf1.plotting.driver_color(driver2Code)
+    color1 = fastf1.plotting.get_driver_color(driver1Code, session=session)
     ax.plot(driver1Tel["Distance"], driver1Tel["Speed"], color=color1, label=driver1Code,linewidth=2)
     #driver 2, we'll use dashed line for contrast
-    color2 = fastf1.plotting.driver_color(driver2Code)
-    ax.plot(driver1Tel["Distance"], driver1Tel["Speed"], color=color1, linestyle="--", label=driver1Code,linewidth=2)
+    color2 = fastf1.plotting.get_driver_color(driver2Code, session=session)
+    ax.plot(driver2Tel["Distance"], driver2Tel["Speed"], color=color2, linestyle="--", label=driver2Code,linewidth=2)
 
     #now onto the labels and the grid
     ax.set_xlabel("Distance (m)", fontsize=12)
