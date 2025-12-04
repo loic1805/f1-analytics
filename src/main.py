9d44fbf67fd9fd0ed546079960b0282737ecb400
loic1805 <loicgasikara@gmail.com>
2025-12-04 22:53:02 +0100

    integrated delta analysis

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from telemetry import loadSession, getFastestLap
-from plotter import plotSpeedTrace
+from plotter import plotAnalysis
+from analysis import computeDeltaTime
 
 def main():
     #setup
@@ -13,16 +14,17 @@ def main():
     session = loadSession(year, gp, sessionType)
     if not session:
         return
-    #laps getter
+    #get laps
     d1Lap, d1Tel = getFastestLap(session, driver1)
     d2Lap, d2Tel = getFastestLap(session, driver2)
     if d1Tel is None or d2Tel is None:
         print(f"Couldn't extract telemetry for one or both drivers.")
         return
+    #computing the delta
+    deltaData = computeDeltaTime(d1Tel, d2Tel)
     #now the plotting
-    eventName = f"{session.event.EventName} {session.event.year} - {sessionType}"
-    plotSpeedTrace(session, d1Tel, driver1, d2Tel, driver2)
-    print("Displaying on..")
+    plotAnalysis(session, d1Tel, driver1, d2Tel, driver2, deltaData)
+    print("Comparison report generated !")
     plt.show()
 
 if __name__ == "__main__":
